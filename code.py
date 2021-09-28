3cf7c3a3409cb82fc9bc731de680e3e2704a3a59
BeomSoo-Ann <bumsuan@gmail.com>
2021-09-28 11:12:13 +0900

    BJ no.2775

--- code.py
+++ code.py
@@ -1,32 +1,31 @@
-# 10250 hotel problem
-# H : 층수, W : 층 별 방 갯수, N : 손님 번호
+# 2775 APT problem
+# a층의 b호에 살려면 자신의 아래(a-1)층의 1호부터 b호까지 사람들의 수의 합만큼 사람들을 데려와 살아야 한다
+# floor : n >= 1  room : k <= 14
+# ground floor --> 1 2 3 4 ... 14
+# first floor --> 1 3 6 10 ... 105
+# second floow --> 1 4 10 20 ...
 
-import sys
+T = int(input())
 
-N = int(input())
+ground_floor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
-for i in range(N):
-    H, W, N = map(int, sys.stdin.readline().split())
 
-    if H == 1:
-        floor_num = 1
-        room_num = N
+def roommate(list_in):
+    next_floor = []
+    tot = 0
+    for i in range(14):
+        tot += list_in[i]
+        next_floor.append(tot)
+    return next_floor
 
-    elif W == 1:
-        floor_num = N
-        room_num = 1
 
-    elif H != 1 and W != 1:
-        floor_num = N % H
-        room_num = N//H + 1
-
-        if floor_num == 0:
-            floor_num = H
-            room_num -= 1
-        else:
-            pass
-
-    else:
-        pass
-
-    print(floor_num*100 + room_num)
+for i in range(T):
+    k = int(input())
+    n = int(input())
+    next_floor = roommate(ground_floor)
+    if k == 1:
+        print(next_floor[n-1])
+    elif k != 1:
+        for i in range(k-1):
+            next_floor = roommate(next_floor)
+        print(next_floor[n-1])
