1b8bc7792ae79c5f8be56d6987124818f4d32e68
BeomSoo-Ann <bumsuan@gmail.com>
2021-09-29 15:44:41 +0900

    BJ no.2839 unsolved

--- code.py
+++ code.py
@@ -1,31 +1,11 @@
-# 2775 APT problem
-# a층의 b호에 살려면 자신의 아래(a-1)층의 1호부터 b호까지 사람들의 수의 합만큼 사람들을 데려와 살아야 한다
-# floor : n >= 1  room : k <= 14
-# ground floor --> 1 2 3 4 ... 14
-# first floor --> 1 3 6 10 ... 105
-# second floow --> 1 4 10 20 ...
+# 2839 suger problem
+# N = 5x + 3y
 
-T = int(input())
-
-ground_floor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+N = int(input())
 
 
-def roommate(list_in):
-    next_floor = []
-    tot = 0
-    for i in range(14):
-        tot += list_in[i]
-        next_floor.append(tot)
-    return next_floor
+def suger(N):
+    return
 
 
-for i in range(T):
-    k = int(input())
-    n = int(input())
-    next_floor = roommate(ground_floor)
-    if k == 1:
-        print(next_floor[n-1])
-    elif k != 1:
-        for i in range(k-1):
-            next_floor = roommate(next_floor)
-        print(next_floor[n-1])
+suger(N)
