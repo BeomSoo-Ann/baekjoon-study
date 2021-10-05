e28a6cc4d813defa091e87c83d69560d2ee0d308
BeomSoo-Ann <bumsuan@gmail.com>
2021-10-05 17:26:30 +0900

    BJ no.1011 unsolved

--- code.py
+++ code.py
@@ -1,5 +1,25 @@
-# 10757 HUGE number add
+# 1011 Interstellar
+# 3 steps --> 1-1-1 3/0
+# 4 steps --> 1-2-1 3/1
+# 5 steps --> 1-2-1-1 4/1
+# 6 steps --> 1-2-2-1 4/2
+# 7 steps --> 1-2-1-2-1 5/2/0
+# 8 steps --> 1-2-2-2-1 5/3/0
+# 9 steps --> 1-2-3-2-1 5/3/1
+# 10 steps --> 1-2-2-2-2-1 6/4/0
+# 11 steps --> 1-2-3-2-2-1 6/4/1
+# steps --> 3/1 < 4/2 < 5/3/1 < 6/4/2 < 7/5/3/1
+# like a pyramid
+
+
 import sys
 
-a, b = map(int, sys.stdin.readline().split())
-print(a+b)
+start_point = 1
+
+T = int(input())
+
+for i in range(T):
+    x, y = map(int, sys.stdin.readline().split())
+    steps = y - x
+
+    print()
