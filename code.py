a475e4fad3dc10d4c108a2a05956fc98214cc78d
BeomSoo-Ann <bumsuan@gmail.com>
2021-10-06 15:33:54 +0900

    BJ no.1011 unsolved_2

--- code.py
+++ code.py
@@ -1,22 +1,20 @@
 # 1011 Interstellar
-# 3 steps --> 1-1-1 3/0
-# 4 steps --> 1-2-1 3/1
-# 5 steps --> 1-2-1-1 4/1
-# 6 steps --> 1-2-2-1 4/2
-# 7 steps --> 1-2-1-2-1 5/2/0
-# 8 steps --> 1-2-2-2-1 5/3/0
-# 9 steps --> 1-2-3-2-1 5/3/1
-# 10 steps --> 1-2-2-2-2-1 6/4/0
-# 11 steps --> 1-2-3-2-2-1 6/4/1
-# steps --> 3/1 < 4/2 < 5/3/1 < 6/4/2 < 7/5/3/1
+# 1 step --> 1
+# 2 steps --> 1-1 2 `2`
+# 3 steps --> 1-2-1 3/1 `4`
+# 4 steps --> 1-2-2-1 4/2 `6`
+# 5 steps --> 1-2-3-2-1 5/3/1 `9`
+# 6 steps --> 1-2-3-3-2-1 6/4/2 `12`
+# 7 steps --> 1-2-3-4-3-2-1 7/5/3/1 `16`
+# 8 steps --> 1-2-3-4-4-3-2-1 8/6/4/2 `20`
+# 9 steps --> 1-2-3-4-5-4-3-2-1 9/7/5/3/1 `25`
 # like a pyramid
-
 
 import sys
 
-start_point = 1
-
 T = int(input())
+
+# found logic but no idea to create code
 
 for i in range(T):
     x, y = map(int, sys.stdin.readline().split())
