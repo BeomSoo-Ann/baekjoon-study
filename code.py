13e9b4cf628b9231f56eb2809850ff02d040b0dd
BeomSoo-Ann <bumsuan@gmail.com>
2021-12-30 16:44:49 +0900

    No.3009 Solved

--- code.py
+++ code.py
@@ -1,8 +1,25 @@
-# No.1085 Out of Square
+# No.3009
 
 from sys import stdin
 
-x, y, w, h = map(int, stdin.readline().split())
+x = list(map(int, stdin.readline().split()))
+y = list(map(int, stdin.readline().split()))
+z = list(map(int, stdin.readline().split()))
 
-dist_list = [x, y, w-x, h-y]
-print(min(dist_list))
+loc_x = [x[0], y[0], z[0]]
+loc_y = [x[1], y[1], z[1]]
+
+loc_x.sort()
+loc_y.sort()
+
+if loc_x.count(loc_x[0]) == 1:
+    w_x = loc_x[0]
+else:
+    w_x = loc_x[2]
+
+if loc_y.count(loc_y[0]) == 1:
+    w_y = loc_y[0]
+else:
+    w_y = loc_y[2]
+
+print(w_x, w_y)
