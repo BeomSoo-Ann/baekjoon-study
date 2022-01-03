f2c77733e5a8a2fe915cea5daa8dec97edf2cd86
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-03 11:04:05 +0900

    No.4153 Solved

--- code.py
+++ code.py
@@ -1,7 +1,15 @@
 # 4153
 
-from sys import stdin
+from sys import stdin, exit
 
-len_list = list(map(int, stdin.readline().split()))
+while True:
+    len_list = list(map(int, stdin.readline().split()))
 
-print(len_list)
+    len_list.sort()
+
+    if len_list[2]**2 == len_list[0]**2 + len_list[1]**2 and 0 not in len_list:
+        print("right")
+    elif len_list[0] == 0 and len_list[1] == 0 and len_list[2] == 0:
+        break
+    else:
+        print("wrong")
