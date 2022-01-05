e40b409bede9dffc61fce97f38a51bea89db30b6
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-05 15:15:41 +0900

    No.1002 Solved
    need some improve

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 # 1002
 
-from sys import stdin, exit
+from sys import stdin
 
 T = int(stdin.readline())
 
@@ -9,17 +9,26 @@ for i in range(T):
 
     t = ((x_1 - x_2) ** 2 + (y_1 - y_2) ** 2) ** 0.5
 
-    if t != 0:
-        if float(t) < float(r_1 + r_2):
-            print(2)
-        elif float(t) == float(r_1 + r_2):
-            print(1)
-        else:
-            print(0)
-    elif t == 0:
+    r = float(r_1 + r_2)
+    r_list = [r_1, r_2]
+
+    if t == 0:
         if r_1 == r_2:
             print(-1)
-        elif r_1 != r_2:
+        else:
             print(0)
+
+    elif t != 0:
+        if r < t:
+            print(0)
+        elif r == t:
+            print(1)
+        else:
+            if abs(r_1 - r_2) == t:
+                print(1)
+            elif max(r_list) > t + min(r_list):
+                print(0)
+            else:
+                print(2)
     else:
-        exit()
+        pass
