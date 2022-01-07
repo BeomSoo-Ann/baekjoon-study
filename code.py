5fea987a665aa8eadeb12139cc6d1b292fc3a36d
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-07 16:40:51 +0900

    Update code.py
    Need to reduce time complexity, but have no idea

--- code.py
+++ code.py
@@ -7,14 +7,14 @@ system('clear')
 
 
 def star(n):
+    pattern_1 = ['*', '*', '*']
+    pattern_2 = ['*', ' ', '*']
+
     if n == 3:
-        return [['*', '*', '*'], ['*', ' ', '*'], ['*', '*', '*']]
-    else:
-        pattern_1 = [star(n//3)[i] * 3 for i in range(n//3)]
-        pattern_2 = [star(n//3)[i] + [' ' * (n//3)] + star(n//3)[i]
-                     for i in range(n//3)]
         return pattern_1 + pattern_2 + pattern_1
+    else:
+        line_1 = []
+        return
 
 
-for i in star(int(stdin.readline()))[::-1]:
-    print(''.join(i))
+print(star(3))
