e8d612a245a1652d300b7bdafe34ac963bd31de1
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-10 16:57:43 +0900

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 # 2447 별 찍기 - 10
+# first code time complexity --> O(n**2)
 
 from sys import stdin
 from os import system
@@ -7,14 +8,17 @@ system('clear')
 
 
 def star(n):
-    pattern_1 = ['*', '*', '*']
-    pattern_2 = ['*', ' ', '*']
-
     if n == 3:
-        return pattern_1 + pattern_2 + pattern_1
+        base = [['*', '*', '*'], ['*', ' ', '*'], ['*', '*', '*']]
+        return base
     else:
-        line_1 = []
-        return
+        pattern_1 = [star(n//3)[i] * 3 for i in range(n//3)]
+        pattern_2 = [star(n//3)[i] + [' '] * (n//3) + star(n//3)[i]
+                     for i in range(n//3)]
+        return pattern_1 + pattern_2 + pattern_1
 
 
-print(star(3))
+N = int(stdin.readline())
+
+for i in range(N):
+    print(''.join(star(N)[i]))
