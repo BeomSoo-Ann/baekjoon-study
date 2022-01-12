1216e176ec68a28fa35a8c8b2a961e506b39cfbe
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-12 17:32:37 +0900

    Update code.py

--- code.py
+++ code.py
@@ -11,15 +11,16 @@ def star(n):
     if n == 3:
         return ['***', '* *', '***']
     else:
-        pattern_1 = [star(n//3)[i] * 3 for i in range(n//3)]
-        pattern_2 = [star(n//3)[i] + ' ' * (n//3) + star(n//3)[i]
-                     for i in range(n//3)]
+        pattern_1 = []
+        pattern_2 = []
+
+        for i in range(n//3):
+            pattern_1.append(star(n//3)[i] * 3)
+            pattern_2.append(star(n//3)[i] + ' ' * (n//3) + star(n//3)[i])
         return pattern_1 + pattern_2 + pattern_1
 
 
 N = int(stdin.readline())
-
-print(star(N))
 
 for j in range(N):
     print(''.join(star(N)[j]))
