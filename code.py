2a8057c211e977dddedf420a9afa1bad83927377
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-14 10:47:32 +0900

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 # 2447 별 찍기 - 10
-# first code time complexity --> O(n**2)
+# first code time complexity --> O(n**3)
+# next code time complexity --> O(n**2)
 
 from sys import stdin
 from os import system
@@ -20,7 +21,25 @@ def star(n):
         return pattern_1 + pattern_2 + pattern_1
 
 
-N = int(stdin.readline())
+# N = int(stdin.readline())
+#
+# for j in range(N):
+#     print(''.join(star(N)[j]))
+#
+##################################################################################
 
-for j in range(N):
-    print(''.join(star(N)[j]))
+# 2447 별 찍기 - 10
+
+
+def star_2(n):
+    if n == 3:
+        return ['***', '* *', '***']
+    else:
+        pattern_1 = [star_2(n//3)[i] * 3 for i in range(n//3)]
+        pattern_2 = [star_2(n//3)[i] + ' ' * (n//3) + star_2(n//3)[i]
+                     for i in range(n//3)]
+        return pattern_1 + pattern_2 + pattern_1
+
+
+for i in star_2(int(stdin.readline()))[::-1]:
+    print(''.join(i))
