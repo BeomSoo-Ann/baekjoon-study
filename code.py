9b564ca71d261507c09c55d7e9d71419726e001d
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-14 17:33:28 +0900

    Update code.py

--- code.py
+++ code.py
@@ -21,25 +21,7 @@ def star(n):
         return pattern_1 + pattern_2 + pattern_1
 
 
-# N = int(stdin.readline())
-#
-# for j in range(N):
-#     print(''.join(star(N)[j]))
-#
-##################################################################################
+N = int(stdin.readline())
 
-# 2447 별 찍기 - 10
-
-
-def star_2(n):
-    if n == 3:
-        return ['***', '* *', '***']
-    else:
-        pattern_1 = [star_2(n//3)[i] * 3 for i in range(n//3)]
-        pattern_2 = [star_2(n//3)[i] + ' ' * (n//3) + star_2(n//3)[i]
-                     for i in range(n//3)]
-        return pattern_1 + pattern_2 + pattern_1
-
-
-for i in star_2(int(stdin.readline()))[::-1]:
-    print(''.join(i))
+for j in star(N):
+    print(''.join(j))
