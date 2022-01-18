d863ea53a549dc1f5548c2e6f070ea4702ba43eb
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-18 10:42:37 +0900

    No.11729 Solved

--- code.py
+++ code.py
@@ -1,64 +1,27 @@
-# 2447 별 찍기 - 10
-# first code time complexity --> O(n**3)
-# next code time complexity --> O(n**2)
+# 11729 Hanoi Tower
 
-from sys import setrecursionlimit
 from sys import stdin
-from os import system
 
-system('clear')
-
-setrecursionlimit(10**6)
+step = []
 
 
-def star(n):
-    if n == 3:
-        return ['***', '* *', '***']
+def move_hanoi(n, start, end, temp):
 
+    if n == 1:
+        step.append([start, end])
     else:
-        stars = star(n//3)
+        move_hanoi(n-1, start, temp, end)
+        step.append([start, end])
+        move_hanoi(n-1, temp, end, start)
 
-        pattern = []
-
-        for s in stars:
-            pattern.append(s*3)
-        for s in stars:
-            pattern.append(s+' '*(n//3)+s)
-        for s in stars:
-            pattern.append(s*3)
-
-        return pattern
+    return step
 
 
 N = int(stdin.readline())
 
-for j in star(N):
-    print(''.join(j))
+move_map = move_hanoi(N, 1, 3, 2)
 
+print(len(move_map))
 
-#########################
-#########################
-####### Reference #######
-#########################
-#########################
-
-
-def append_star(LEN):
-    if LEN == 1:
-        return ['*']
-
-    STAR = append_star(LEN//3)
-    L = []
-
-    for S in STAR:
-        L.append(S*3)
-    for S in STAR:
-        L.append(S+' '*(LEN//3)+S)
-    for S in STAR:
-        L.append(S*3)
-
-    return L
-
-
-# n = int(stdin.readline().strip())
-# print('\n'.join(append_star(n)))
+for i in move_map:
+    print(i[0], i[1])
