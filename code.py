c54bdae9976ed2fc90c721fad1debd3b868185da
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-24 10:11:58 +0900

    No.7568 Solved

--- code.py
+++ code.py
@@ -1,22 +1,18 @@
-# 2231 분해합
-# N = a//base + a//(base-1) + ... + a//1
+# 7568 덩치
 
 from sys import stdin
 
 N = int(stdin.readline())
-base = len(str(N))
 
-for i in range(1, N+1):
-    SUM = sum(map(int, str(i)))
-    i_sum = i + SUM
+PARTY = [list(map(int, stdin.readline().split())) for i in range(N)]
 
-    if i_sum == N:
-        print(i)
-        break
+for HUMAN_1 in PARTY:
+    ORDER = 1
 
-    elif i == N:
-        print(0)
-        break
+    for HUMAN_2 in PARTY:
+        if HUMAN_2[0] > HUMAN_1[0] and HUMAN_2[1] > HUMAN_1[1]:
+            ORDER += 1
+        else:
+            pass
 
-    else:
-        pass
+    print(ORDER, end=' ')
