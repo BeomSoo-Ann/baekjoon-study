d1cf68498a339091318ce634dd74eb0ee5781d41
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-24 16:57:45 +0900

    No.1018 Solved

--- code.py
+++ code.py
@@ -15,7 +15,6 @@ SQUARE = [list(map(str, stdin.readline().strip())) for _ in range(M)]
 def COUNT_ERROR(BOARD, BASE):
     cnt = 0
     for i in range(8):
-        cnt = 0
         for j in range(8):
             if BOARD[i][j] != BASE[i][j]:
                 cnt += 1
@@ -24,5 +23,20 @@ def COUNT_ERROR(BOARD, BASE):
     return cnt
 
 
-def MAKE_8X8(BOARD, row_start, col_start):
+def MAKE_8X8(BOARD, col_start, row_start):
     NEW_BOARD = []
+    for i in range(row_start, row_start + 8):
+        NEW_BOARD.append(BOARD[i][col_start:col_start + 8])
+    return NEW_BOARD
+
+
+ERROR_LIST = []
+
+for i in range(M-7):
+    for j in range(N-7):
+        BOARD = MAKE_8X8(SQUARE, j, i)
+        ERROR_LIST.append(COUNT_ERROR(BOARD, BASE_B))
+        ERROR_LIST.append(COUNT_ERROR(BOARD, BASE_W))
+
+
+print(min(ERROR_LIST))
