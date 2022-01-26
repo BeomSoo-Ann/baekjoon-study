34e5079d56ac3b318594acc2b83c3c1ed9fe1c46
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-26 09:07:52 +0900

    No.1436 Solved

--- code.py
+++ code.py
@@ -1,7 +1,22 @@
 # 1436 Movie maker 666
-# 666 1666 2666 3666 4666 5666 6660 6661 6662 6663 6664 6665 6666 6667 6668 6669 7666 8666 9666
+
 
 from sys import stdin
 
-N_st = int(stdin.readline())
+N = int(stdin.readline())
 
+def find_Movie():
+    BASE = 666
+    i = 1
+    
+    if N == 1:
+        return 666
+    else:
+        while True:
+            BASE += 1
+            if '666' in str(BASE):
+                i += 1
+                if i == N:
+                    return BASE
+                
+print(find_Movie())
