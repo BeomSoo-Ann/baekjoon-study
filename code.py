2a98f35f2d07e2933a5a841483b2f21565890f9e
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-26 15:55:33 +0900

    No.2751 Solved

--- code.py
+++ code.py
@@ -1,12 +1,20 @@
-# 2750 order
+#2751 order
 
 from sys import stdin
 
-
 N = int(stdin.readline())
-
 NUM_list = [int(stdin.readline()) for _ in range(N)]
-NUM_list.sort()
 
-for i in range(N):
-    print(NUM_list[i])
+#################### 2750 solution ####################
+
+# NUM_list.sort()
+# 
+# for i in range(N):
+#     print(NUM_list[i])
+
+#################### use .sorted() ####################
+
+for i in sorted(NUM_list):
+    print(i)
+
+# result: 8ms faster, similar to 2750
