8527b81bbe6e35a35b303c4789cff1643c57e393
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-27 15:40:39 +0900

    found why memory error occors

--- code.py
+++ code.py
@@ -3,12 +3,16 @@
 from sys import stdin, stdout
 
 N = int(stdin.readline())
-case = [0] * 10001
+num_list = [int(stdin.readline()) for _ in range(N)]
 
-for _ in range(N):
-    case[int(stdin.readline())] += 1
+maxi = max(num_list)
+
+case = [0] * (maxi + 1)
+
+for num in num_list:
+    case[num] += 1
     
-for i in range(10001):
+for i in range(maxi + 1):
     for j in range(case[i]):
         stdout.write(str(i) + '\n')
-        
+
