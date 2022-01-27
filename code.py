c389807514b9ecfc21a737131fdd07b4812e3a9d
BeomSoo-Ann <bumsuan@gmail.com>
2022-01-27 13:30:39 +0900

    update

--- code.py
+++ code.py
@@ -1,20 +1,36 @@
-#2751 order
+# 10989 ordering 3
 
-from sys import stdin
+from sys import stdin, stdout, setrecursionlimit
+
+setrecursionlimit(10**7)
 
 N = int(stdin.readline())
-NUM_list = [int(stdin.readline()) for _ in range(N)]
+num_list = [int(stdin.readline()) for _ in range(N)]
 
-#################### 2750 solution ####################
 
-# NUM_list.sort()
-# 
-# for i in range(N):
-#     print(NUM_list[i])
+L = len(num_list)
+maxi = max(num_list)
 
-#################### use .sorted() ####################
 
-for i in sorted(NUM_list):
-    print(i)
+count_base = [0] * (maxi + 1)
 
-# result: 8ms faster, similar to 2750
+for i in range(L):
+    count_base[num_list[i]] += 1
+
+
+count_sum = []
+count_sum.append(count_base[0])
+
+for i in range(1, maxi + 1):
+    count_sum.append(count_sum[i - 1] + count_base[i])
+
+
+list_sorted = [0] * (L+1)
+    
+for i in range(L-1, -1, -1):
+    list_sorted[count_sum[num_list[i]]] = num_list[i]
+    count_sum[num_list[i]] -= 1
+
+
+for i in list_sorted[1:]:
+    stdout.write(str(i) + '\n')
