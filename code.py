3c7734115e4f601c9e9456bcee59e95086a9673f
BeomSoo-Ann <bumsuan@gmail.com>
2022-02-03 14:11:29 +0900

    No.2108 Solved

--- code.py
+++ code.py
@@ -1,18 +1,67 @@
-# 10989 ordering 3
+# 2108 Statistic
 
-from sys import stdin, stdout
+
+from sys import stdin
 
 N = int(stdin.readline())
 num_list = [int(stdin.readline()) for _ in range(N)]
 
-maxi = max(num_list)
+maxi_list = max(num_list)
+mini_list = min(num_list)
 
-case = [0] * (maxi + 1)
+def arithmetic_mean(L, n):
+    S = sum(L)
+    mean_num = S / n
+    print(round(mean_num))
 
-for num in num_list:
-    case[num] += 1
+
+def median(L, n):
+    L.sort()
+    if n % 2 == 0:
+        median_num = (L[n//2] + L[n//2 - 1]) / 2
+        print(median_num)
+    else:
+        median_num = L[n//2]
+        print(median_num)
+        
+
+def find_mode(L, n):
+    L.sort()
+    cnt = 1
+    cnt_list = [1]
     
-for i in range(maxi + 1):
-    for j in range(case[i]):
-        stdout.write(str(i) + '\n')
+    for i in range(1, n):
+        if L[i] == L[i-1]:
+            cnt += 1
+            cnt_list.append(cnt)
+        else:
+            cnt = 1
+            cnt_list.append(cnt)
+    
+    max_cnt = max(cnt_list)
 
+    index_list = []
+    
+    for i in cnt_list:
+        if i == max_cnt:
+            index_list.append(cnt_list.index(i))
+            cnt_list[cnt_list.index(i)] = 0
+        else:
+            pass
+    
+    if len(index_list) == len(cnt_list):
+        if len(cnt_list) == 1:
+            print(L[0])
+        else:
+            print(L[1])
+    else:
+        if len(index_list) == 1:
+            print(L[index_list[0]])
+        else:
+            print(L[index_list[1]])
+        
+ 
+arithmetic_mean(num_list, N)
+median(num_list, N)
+find_mode(num_list, N)
+print(maxi_list - mini_list)
