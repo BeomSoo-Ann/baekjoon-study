5144c75cf5a2833473ecc053cd1b990e87cb1579
BeomSoo-Ann <bumsuan@gmail.com>
2022-02-04 14:24:26 +0900

    Update

--- code.py
+++ code.py
@@ -1,12 +1,31 @@
-# 11651 Sort the Locations 2
+# 17478 what's recursion function?
 
 from sys import stdin
 
 N = int(stdin.readline())
 
-Loc_list = [list(map(int, stdin.readline().split())) for _ in range(N)]
 
-sorted_list = sorted(Loc_list, key=lambda x: (x[1], x[0]))
+def answer(n):
+    i = N
+    
+    first = '"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.'
+    second = '마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.'
+    third = '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."'
+    
+    if n == 0:
+        print('____'*i +'"재귀함수가 뭔가요?"')
+        print('____'*i +'"재귀함수는 자기 자신을 호출하는 함수라네"')
+        print('____'*i + '라고 답변하였지.')
+        return
+    else:
+        print('____'*(i-n) +'"재귀함수가 뭔가요?"')
+        print('____'*(i-n) + first)
+        print('____'*(i-n) + second)
+        print('____'*(i-n) + third)
+        answer(n-1)
+        print('____'*(i-n) + '라고 답변하였지.')
+    
 
-for i in sorted_list:
-    print(i[0], i[1])
+
+print('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.')
+answer(N)
