ff9cf3959e048a619ce8dbe70f6e9b00c1cd910e
BeomSoo-Ann <bumsuan@gmail.com>
2022-02-04 15:03:15 +0900

    No.1181 Solved

--- code.py
+++ code.py
@@ -1,29 +1,17 @@
-# 17478 what's recursion function?
+# No.1181
 
-from sys import stdin
+from sys import stdin, stdout
 
 N = int(stdin.readline())
+word_list = []
 
-
-def answer(n):
+for _ in range(N):
+    word = stdin.readline().strip()
     
-    first = '"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.'
-    second = '마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.'
-    third = '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."'
-    
-    if n == 0:
-        print('____'*N +'"재귀함수가 뭔가요?"')
-        print('____'*N +'"재귀함수는 자기 자신을 호출하는 함수라네"')
-        print('____'*N + '라고 답변하였지.')
-    else:
-        print('____'*(N-n) +'"재귀함수가 뭔가요?"')
-        print('____'*(N-n) + first)
-        print('____'*(N-n) + second)
-        print('____'*(N-n) + third)
-        answer(n-1)
-        print('____'*(N-n) + '라고 답변하였지.')
-    
+    if word not in word_list:
+        word_list.append(word)
+        
+sorted_list = sorted(word_list, key = lambda x: (len(x), x))
 
-
-print('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.')
-answer(N)
+for word in sorted_list:
+    stdout.write(word + '\n')
