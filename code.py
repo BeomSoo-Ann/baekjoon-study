829e51eed8b1a10dcd5df24393231503bc9354e9
BeomSoo-Ann <bumsuan@gmail.com>
2022-02-08 14:46:54 +0900

    No.15649 Solved

--- code.py
+++ code.py
@@ -3,7 +3,27 @@
 from sys import stdin
 
 N, M = map(int, stdin.readline().split())
+L = []
 
-base = [i for i in range(1, N+1)]
 
-print(base)
+def backtracking_num():
+    if len(L) == M:
+        print(' '.join(map(str, L)))
+        return
+
+    for i in range(1, N+1):
+        if i in L:
+            continue
+
+        L.append(i)
+
+        if len(L) != 1:
+            if L[-1] < L[-2]:
+                L.pop()
+                continue
+
+        backtracking_num()
+        L.pop()
+
+
+backtracking_num()
