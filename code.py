e25e2d6ab5f598f7de321d6c05b833c052b9d2c0
BeomSoo-Ann <bumsuan@gmail.com>
2022-02-14 17:33:34 +0900

    Update

--- code.py
+++ code.py
@@ -4,55 +4,32 @@ from sys import stdin
 
 
 N = int(stdin.readline())
-num_list = list(map(int, stdin.readline().split()))
-arithmetic_count = list(map(int, stdin.readline().split()))
+#teamstate_list = [list(map(int, stdin.readline().split())) for _ in range(N)]
+# print(teamstate_list)
 L = []
-tot_list = []
+L_list = []
 
 
-def add(a, b):
-    return a + b
-
-
-def sub(a, b):
-    return a - b
-
-
-def mul(a, b):
-    return a * b
-
-
-def div(a, b):
-    if a < 0 or b < 0:
-        return (abs(a)//abs(b))*(-1)
-    else:
-        return a // b
-
-
-def backtracking():
-    if len(L) == N-1:
-        calc_list = []
-        calc_list.append(num_list[0])
-        for i in range(N-1):
-            if L[i] == 0:
-                calc_list.append(add(calc_list[-1], num_list[i+1]))
-            elif L[i] == 1:
-                calc_list.append(sub(calc_list[-1], num_list[i+1]))
-            elif L[i] == 2:
-                calc_list.append(mul(calc_list[-1], num_list[i+1]))
-            elif L[i] == 3:
-                calc_list.append(div(calc_list[-1], num_list[i+1]))
-        tot_list.append(calc_list[N-1])
+def backtrace():
+    global L
+    if len(L) == N//2:
+        print(L)
         return
 
-    for i in range(4):
-        if arithmetic_count[i] > 0:
-            if L.count(i) < arithmetic_count[i]:
-                L.append(i)
-                backtracking()
+    for i in range(N):
+        if i+1 in L:
+            continue
+        L.append(i+1)
+
+        if len(L) != 1:
+            if L[-1] < L[-2]:
                 L.pop()
+                continue
+
+        backtrace()
+        L_list.append(L)
+        L.pop()
 
 
-backtracking()
-print(max(tot_list))
-print(min(tot_list))
+backtrace()
+print(L_list)
