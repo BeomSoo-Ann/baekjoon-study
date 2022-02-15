c5a2655c3235205392ea4947a0c20b65781da626
BeomSoo-Ann <bumsuan@gmail.com>
2022-02-15 16:05:40 +0900

    No.1003 Solved

--- code.py
+++ code.py
@@ -1,6 +1,23 @@
 # No.1003 fibonacci
 
-from sys import stdin
+from sys import stdin, stdout
 
 
-N = int(stdin.readline())
+T = int(stdin.readline())
+
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+for _ in range(T):
+    N = int(stdin.readline())
+    if N == 0:
+        print(1, 0)
+        continue
+    print(fibonacci(N-1), fibonacci(N))
