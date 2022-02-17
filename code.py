20c6bcf5d151c0be01740c9d70773ae3020bfdbb
BeomSoo-Ann <bumsuan@gmail.com>
2022-02-17 15:31:37 +0900

    No.9184 Solved

--- code.py
+++ code.py
@@ -2,36 +2,35 @@
 
 from sys import stdin, stdout
 
+dp = [[[0]*21 for _ in range(21)] for _ in range(21)]
 
 
 def w(a, b, c):
-    if a <=0 or b <= 0 or c <= 0:
+
+    if a <= 0 or b <= 0 or c <= 0:
         return 1
-    
+
     elif a > 20 or b > 20 or c > 20:
         return 1048576
-    
+
+    elif dp[a][b][c]:
+        return dp[a][b][c]
+
     elif a == b == c:
         return 2**a
-    
-    elif b > c:
-        return w(a, c, b)
-    
-    elif a < b and b < c:
-        return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
-    
-    elif a == 1:
-        return 2
-    
-    else:
-        return w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+
+    elif a < b < c:
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return dp[a][b][c]
+
+    dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + \
+        w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    return dp[a][b][c]
 
 
 while True:
     A, B, C = map(int, stdin.readline().split())
     if A == B == C == -1:
         break
-    
-    print(w(A, B, C))
-    
-    
+
+    print(f"w({A}, {B}, {C}) = {w(A, B, C)}")
