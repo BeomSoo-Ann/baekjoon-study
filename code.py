5c31e09d0cdb849c564c9008288d4d7bc967c6ae
BeomSoo-Ann <bumsuan@gmail.com>
2022-03-14 17:13:29 +0900

    No.1463 Solved

--- code.py
+++ code.py
@@ -1,29 +1,49 @@
-# No.2579 stairs
+# No.1463 OUTPUT 1
+
 
 from sys import stdin
 
 
 N = int(stdin.readline())
-stair = [0] * 300
-for i in range(N):
-    stair[i] = int(stdin.readline())
+dp = [0] * (10**6 + 1)
 
-dp = [0] * 300
 
-dp[0] = stair[0]
-dp[1] = stair[0] + stair[1]
-dp[2] = stair[2] + max(dp[1], dp[0])
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+dp[4] = 2
+dp[5] = 3
+dp[6] = 2
+dp[7] = 3
+dp[8] = 3
+dp[9] = 2
+dp[10] = 3
 
-if N == 1:
-    print(dp[0])
-elif N == 2:
-    print(dp[1])
-elif N == 3:
-    print(dp[2])
-else:
-    for i in range(3, N):
-        dp[i] = max(stair[i]+dp[i-2], stair[i]+dp[i-3]+stair[i-1])
 
-    print(dp)
-    print(dp[N-1])
-    
+if N <= 10:
+    print(dp[N])
+
+elif N > 10:
+    for i in range(11, N+1):
+        dp[i] = dp[i-1]+1
+
+        if i % 3 == 0:
+            dp[i] = min(dp[i//3]+1, dp[i])
+        if i % 2 == 0:
+            dp[i] = min(dp[i//2]+1, dp[i])
+
+    print(dp[N])
+############################################
+n = int(input())
+
+dp = [0] * (n+1)
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1
+
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+
+print(dp[n])
