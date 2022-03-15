05dcb79b3a7754e14d4c8c0666e035db0f1522b1
BeomSoo-Ann <bumsuan@gmail.com>
2022-03-15 10:04:48 +0900

    No.2156 Solved

--- code.py
+++ code.py
@@ -1,49 +1,24 @@
-# No.1463 OUTPUT 1
+# No.2156 Wine tasting
 
 
 from sys import stdin
 
 
 N = int(stdin.readline())
-dp = [0] * (10**6 + 1)
+wines = [int(stdin.readline()) for _ in range(N)]
+dp = [0] * 10001
 
+if N == 1:
+    print(wines[0])
+elif N == 2:
+    print(wines[0] + wines[1])
+else:
+    dp[0] = wines[0]
+    dp[1] = wines[0] + wines[1]
+    dp[2] = max(wines[0] + wines[2], wines[1] + wines[2])
 
-dp[1] = 1
-dp[2] = 1
-dp[3] = 1
-dp[4] = 2
-dp[5] = 3
-dp[6] = 2
-dp[7] = 3
-dp[8] = 3
-dp[9] = 2
-dp[10] = 3
+    for i in range(3, N):
+        dp[i] = max(dp[i-2]+wines[i], max(dp[:i-2]) +
+                    wines[i-1]+wines[i], max(dp))
 
-
-if N <= 10:
-    print(dp[N])
-
-elif N > 10:
-    for i in range(11, N+1):
-        dp[i] = dp[i-1]+1
-
-        if i % 3 == 0:
-            dp[i] = min(dp[i//3]+1, dp[i])
-        if i % 2 == 0:
-            dp[i] = min(dp[i//2]+1, dp[i])
-
-    print(dp[N])
-############################################
-n = int(input())
-
-dp = [0] * (n+1)
-
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + 1
-
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
-
-print(dp[n])
+    print(max(dp))
